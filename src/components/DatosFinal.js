6044053f3c9d2b7f0f962984bd70f25c8e0917ac
Ana <probablynotannie@gmail.com>
2024-08-07 15:40:58 +0200

    añadido formulatio para meter la tarjeta (obviamente no funciona jjjj)

--- src/components/DatosFinal.js
+++ src/components/DatosFinal.js
@@ -14,7 +14,6 @@ function DatosFinal() {
     for (let i = 0; i < datos.length; i++) {
         pasajeros.push({ nombre: datos.nombres[i], apellidos: datos.apellidos[i], DNI: datos.DNI[i] })
     }
-
     return (
         <div className='min-h-[70vh] p-10 dark:bg-slate-900'>
             <h2 className='text-lg font-bold text-center md:text-start dark:text-slate-400'>Datos Reserva</h2>
@@ -32,9 +31,24 @@ function DatosFinal() {
                     </div>
                 ))}
             </div>
-            <section className='mt-5  border-black rounded p-5'>
-                <h3 className='text-md font-semibold text-center md:text-start dark:text-slate-300'>
-                    Selecciona metodo de pago
+            <section className='mt-5  border-black rounded'>
+                <fieldset className='border-2 border-slate-300 dark:border-slate-700'>
+                    <legend className='text-md font-semibold text-center md:text-start dark:text-slate-300 ml-5 text-slate-500'>Págo seguro con tarjeta visa o debito</legend>
+                    <form className='grid grid-cols-12 gap-3 p-5'>
+                        <input className='md:col-span-4 col-span-6 dark:bg-slate-700 dark:text-slate-300 border border-slate-300 dark:border-slate-700' type='text' placeholder='Nombre' />
+                        <input className='md:col-span-4 col-span-6 dark:bg-slate-700 dark:text-slate-300 border border-slate-300 dark:border-slate-700' type='text' placeholder='Apellido/s' />
+                        <input className='md:col-span-4 col-span-6 dark:bg-slate-700 dark:text-slate-300 border border-slate-300 dark:border-slate-700' type='text' placeholder='Nombre' />
+                        <input className='md:col-span-4 col-span-6 dark:bg-slate-700 dark:text-slate-300 border border-slate-300 dark:border-slate-700' type="number" inputmode="numeric" pattern="[0-9\s]{13,19}"
+                            autocomplete="cc-number" maxlength="19"
+                            placeholder="xxxx xxxx xxxx xxxx" required />
+                        <input className='md:col-span-1 col-span-3 dark:bg-slate-700 dark:text-slate-300 border border-slate-300 dark:border-slate-700' type='number' placeholder='Mes exp.' />
+
+                        <input className='md:col-span-1 col-span-3 dark:bg-slate-700 dark:text-slate-300 border border-slate-300 dark:border-slate-700' type='number' placeholder='Año exp.' />
+                        <input name="credit-expires" className="cc-expires md:col-span-2 col-span-6 dark:bg-slate-700 dark:text-slate-300 border border-slate-300 dark:border-slate-700" type="tel" pattern="\d*" maxlength="4" placeholder="CVV" />
+                    </form>
+                </fieldset>
+                <h3 className='text-md font-semibold text-center md:text-start dark:text-slate-300 mt-5'>
+                    Págo express
                 </h3>
                 <div className='flex flex-col md:flex-row  gap-4 mt-5 items-center'>
                     <div className='h-[44px] hover:bg-yellow-500 bg-yellow-400 font-semibold p-2 w-[240px] flex items-center gap-2 cursor-pointer shadow-2xl rounded border-2 border-yellow-500'>
--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -4,7 +4,7 @@ import { faEuroSign, faEnvelope, faFaceLaughWink } from '@fortawesome/free-solid
 function info() {
     return (
         <>
-            <section className='grid md:flex-row flex-col  gap-5 md:gap-5  grid-cols-12 mt-5 bg-slate-100 dark:bg-slate-800 p-10 rounded shadow-lg  '>
+            <section className='grid md:flex-row flex-col  gap-5 md:gap-5  grid-cols-12 mt-5 bg-slate-100 dark:bg-slate-800 p-10 rounded shadow-lg border border-transparent dark:hover:border-indigo-600 transition '>
                 <div className='flex flex-col  text-indigo-600 dark:text-indigo-400 col-span-12 md:col-span-4'>
                     <FontAwesomeIcon className=" text-2xl" icon={faEuroSign} />
                     <p className='font-semibold px-16 text-center md:px-0  text-indigo-500 dark:text-indigo-400'>
@@ -24,7 +24,6 @@ function info() {
                     </p>
                 </div>
             </section>
-
             <section className='mt-10 grid grid-cols-12 gap-5'>
                 <figure className="md:flex group bg-slate-100 dark:bg-slate-800 dark:hover:border border border-transparent dark:hover:border-indigo-600  col-span-12 md:col-span-4 rounded-xl p-8 md:p-0  hover:bg-indigo-500 duration-300">
                     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
@@ -37,7 +36,7 @@ function info() {
                             <div className="text-sky-400 ">
                                 Medio ambiente
                             </div>
-                            <div className="text-slate-500 group-hover:text-slate-300 dark:text-slate-200 ">
+                            <div className="text-slate-500 group-hover:text-slate-300 dark:text-slate-300 ">
                                 Cuida de nuestro planeta
                             </div>
                         </figcaption>
@@ -54,7 +53,7 @@ function info() {
                             <div className="text-sky-400 ">
                                 Precios
                             </div>
-                            <div className="text-slate-500 group-hover:text-slate-300 dark:text-slate-200  ">
+                            <div className="text-slate-500 group-hover:text-slate-300 dark:text-slate-300  ">
                                 Mejores precios garantizados
                             </div>
                         </figcaption>
@@ -71,7 +70,7 @@ function info() {
                             <div className="text-sky-400 ">
                                 Atención al cliente
                             </div>
-                            <div className="text-slate-500 group-hover:text-slate-300 dark:text-slate-200">
+                            <div className="text-slate-500 group-hover:text-slate-300 dark:text-slate-300">
                                 Agentes disponibles 24/7
                             </div>
                         </figcaption>
